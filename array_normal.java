be556e30aa72e6d58942c700b7f9f0603ddb8142
Harshal Kulkarni <harshalkulkarni580@gmail.com>
2026-08-18 13:24:51 +0530

    java Programs

---
+++ array_normal.java
@@ -0,0 +1,12 @@
+class array_normal{
+public static void main(String[] args){
+	int x[]={10,20,30,40};
+
+System.out.println(x[0]);
+System.out.println(x[1]);
+System.out.println(x[2]);
+System.out.println(x[3]);
+}
+}
+
+	
